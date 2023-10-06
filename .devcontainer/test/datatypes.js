4980bcce7e1209ec68c1fbecf13a2424de0f52e1
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-06 18:07:19 +0000

    datatypes

---
+++ .devcontainer/test/datatypes.js
@@ -0,0 +1,2 @@
+"use strict" // Means Treat all js code as newer version
+//alert(3+3) we are using nodeJs not  Browser
---
+++ .devcontainer/test/variables.js
@@ -0,0 +1,17 @@
+console.log("Hello Suraj Singh")
+const AccountId=144553
+let AccountEmail="ssingh.8721382@gmail.com"
+var AccountPassword="12345"
+AccountCity="Delhi"
+
+// AccountId=2 Not Allowed
+
+AccountEmail="surajs2612btechcse2024@kccitm.edu.in" //Allowed
+AccountPassword="54321" //Allowed
+AccountCity="Bihar" //Allowed
+let AccountState;
+/*
+Prefer not to var
+*/
+
+console.table([AccountId,AccountEmail,AccountPassword,AccountCity,AccountState]);
