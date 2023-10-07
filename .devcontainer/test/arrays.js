55ca341c383edcc110b27cb9bc59a877191aede1
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-07 19:12:34 +0000

    slice and splice

---
+++ .devcontainer/test/arrays.js
@@ -0,0 +1,9 @@
+let myarr=[1,2,3,4,5,6,7,8]
+const myarr2=[0,1,2,3,4,5]
+console.log(myarr.slice(1,4))
+console.log(myarr2.slice(1,4))
+console.log(myarr.splice(1,4))
+console.log(myarr2.splice(1,4))  /*splice and slice has diffrence of tacking out 
+                                    sliand coping out respectevely of indexes given of arrays*/
+console.log(myarr)
+console.log(myarr2)
