5cafacf67573a238c33f07cb497484deb6c71c24
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-07 17:00:25 +0000

    math

--- .devcontainer/test/mathandnumbers.js
+++ .devcontainer/test/mathandnumbers.js
@@ -1,9 +1,9 @@
-// // const Score=400
-// // console.log(Score) //output will be 400
-// // const balance=new Number(100) 
-// // console.log(balance)  //output will be [Number:100]
-// // console.log(balance.toString(),balance.length) //output is 100 undefined because toString is Temporary
-// // console.log(balance.toString().length,balance.toString()) //output will be 3 100
+const Score=400
+console.log(Score) //output will be 400
+const balance=new Number(100) 
+console.log(balance)  //output will be [Number:100]
+console.log(balance.toString(),balance.length) //output is 100 undefined because toString is Temporary
+console.log(balance.toString().length,balance.toString()) //output will be 3 100
 
 // const mrp=103.4
 // console.log(mrp.toFixed(2))    
