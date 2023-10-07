d25688486207439000cdb9cc3a58f1d0b196b6b1
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-07 19:03:53 +0000

    date time

---
+++ .devcontainer/test/datetime.js
@@ -0,0 +1,9 @@
+let curdate=new Date()
+console.log(curdate.toString())
+console.log(curdate.toDateString())
+console.log(curdate.toLocaleDateString())
+console.log(curdate.toLocaleString())
+
+newdate=new Date()
+
+console.log(newdate)
