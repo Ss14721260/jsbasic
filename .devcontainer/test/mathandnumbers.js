579d81338bf2eff12ff8f0049e0dadea8cf5c848
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-07 16:51:09 +0000

    randomfunction of math

--- .devcontainer/test/mathandnumbers.js
+++ .devcontainer/test/mathandnumbers.js
@@ -1,15 +1,23 @@
-// const Score=400
-// console.log(Score) //output will be 400
-// const balance=new Number(100) 
-// console.log(balance)  //output will be [Number:100]
-// console.log(balance.toString(),balance.length) //output is 100 undefined because toString is Temporary
-// console.log(balance.toString().length,balance.toString()) //output will be 3 100
+// // const Score=400
+// // console.log(Score) //output will be 400
+// // const balance=new Number(100) 
+// // console.log(balance)  //output will be [Number:100]
+// // console.log(balance.toString(),balance.length) //output is 100 undefined because toString is Temporary
+// // console.log(balance.toString().length,balance.toString()) //output will be 3 100
 
-const mrp=103.4
-console.log(mrp.toFixed(2))    
-console.log(typeof mrp)
-console.log(mrp.toPrecision(8))
+// const mrp=103.4
+// console.log(mrp.toFixed(2))    
+// console.log(typeof mrp)
+// console.log(mrp.toPrecision(8))
 
-const retail=mrp.toFixed(0)
-console.log(retail)
-console.log(Number.isNaN()) //false
+// const retail=mrp.toFixed(0)
+// console.log(retail)
+// console.log(Number.isNaN()) //false
+// const hund=1000000000
+// console.log(hund.toLocaleString('en-IN'))
+const max=6
+const min=1
+// start ludo
+while(true){
+    console.log(console.log(Math.floor((Math.random()*(max-min+1)))+min))
+}
