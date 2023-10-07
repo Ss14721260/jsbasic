90259f50785823883f5d967cab1d84b903156a41
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-07 15:22:09 +0000

    comparisions

---
+++ .devcontainer/test/comparisions.js
@@ -0,0 +1,6 @@
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0);  //true
+// strict checking in js is done by === triple equal.
+console.log('4'>=3) //output will be true
+console.log('4'===4) //output will be false because of datatypeare diffrent
--- .devcontainer/test/datatypes.js
+++ .devcontainer/test/datatypes.js
@@ -1,2 +1,10 @@
 "use strict" // Means Treat all js code as newer version
 //alert(3+3) we are using nodeJs not  Browser
+let name="suraj"
+let age=22      // number =>2 to the power 52
+let bon=true //boolean true / false
+let bigint=2e54 // for greater than 2 to the power 52
+let und;        //undefined
+let son=null    //object
+consol.table([typeof name,age,bon])
+
