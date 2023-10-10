77d9f2dbbc8d2bb73d420bbcf468086973b0c859
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-10 19:10:30 +0000

    array from diffrent objects

--- .devcontainer/test/arrays2.js
+++ .devcontainer/test/arrays2.js
@@ -11,3 +11,8 @@ console.log(alnh)
 // now lets do something of flat()
 ar=[1,2,[3,4,5],6,[7,8,9,[1,2,3,4]]]
 console.log(ar.flat(Infinity))
+
+// array from given object of string
+console.log(Array.from([123,4,5])) //working
+console.log(Array.from(123)) //empty array will be generated
+console.log(Array.from("array from string")) //working
