2f416c66bca2bcb9ef3c39be595ba1e24568cd7e
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-17 04:45:12 +0000

    javascript/while loop

---
+++ javascript/while.js
@@ -0,0 +1,12 @@
+let i=0
+while (true) {
+    console.log('Hello inside the while loop');
+    i++
+    if (i==5) {
+        break
+    }
+}
+do {
+    let a=4;
+    console.log('good to go in while');
+} while (a=0);
