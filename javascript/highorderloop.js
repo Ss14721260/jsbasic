90f4dbf86164cb298b310265e55c1d2b8f72c58d
Suraj Singh <125239925+Ss14721260@users.noreply.github.com>
2023-10-24 10:09:12 +0000

    (for of) and (for in ) loop

--- javascript/highorderloop.js
+++ javascript/highorderloop.js
@@ -1,6 +1,47 @@
 //for of loop
 superheros=['batman','superman','flash','fisherman','wonderwomen']
 for (const elements of superheros) {
-    console.log(elements);
+    // console.log(elements);
 }
 //for in loop
+for (const i in superheros) {
+    if (superheros.hasOwnProperty.call(superheros, i)) {
+        const element = superheros[i];
+        // console.log('#',element);
+        
+    }
+}
+// Note that .hasOwnProperty.call function  is not know for me at this position
+ 
+const student={
+    name:'suraj',
+    age:21
+}
+//object is not iterable from (for of loop)
+// for (const data of student) {
+//     console.log(data);
+// }
+
+//map  is like singleton object
+const singletonStudent=new Map();
+singletonStudent.set('name','suraj singh');
+singletonStudent.set('age',21);
+console.log(singletonStudent);
+singletonStudent.set('sex','Male')
+console.log(singletonStudent);
+for (const [i,j] of singletonStudent) {
+    console.log(i,'=>',j); 
+}
+
+//object iterations using (for in) method
+computerlanguages={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+for (const k in computerlanguages) {
+    console.log(k,'=',computerlanguages[k])       
+}
+
+//
